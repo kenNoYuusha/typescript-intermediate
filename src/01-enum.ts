48be3e64a0c9f3c2bae0ee61722422bfc4c21bf5
Ren Amaki <jorge_luis1709@hotmail.com>
2023-01-02 18:07:50 -0600

    enums n tuples

---
+++ src/01-enum.ts
@@ -0,0 +1,18 @@
+//crea un conjunto de opciones predefinidas, y al
+//momento de tipar con ellas, solo puedes elegir entre esas opciones.
+enum ROLES {
+  ADMIN = 'admin',
+  SELLER = "seller",
+  CUSTOMER = "customer",
+}
+
+type User = {
+  username: string;
+  role: ROLES
+}
+
+const luisUser: User = {
+  username: "jluis",
+  role: ROLES.ADMIN
+}
+
---
+++ src/02-tuples.ts
@@ -0,0 +1,11 @@
+//es un arreglo fuertemente tipado
+//tanto en cantidad como en tipo de elementos que contiene
+
+//forma de tipar una tupla
+const user: [string, number] = ['jluis', 12];
+
+
+//tipado de un array convencional
+const people:string[] = ["Luis", "Roberto", "Alfonso"]
+const items:(number | string)[] = [5,9,"lanza", "gema", 5]
+
